33982370ad732ba93d7f20840c2d01f1bf0222c0
Kevin Kim <Kevin.Kim@itg.com>
2018-07-27 16:46:02 -0700

    update searchBar class with constructor and initial state value

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: ''
+		};
+	}
+
 	render() {
 		return (
 			<form className="input-group">
-				<input></input>
+				<input placeholder="Search for city"
+					className="form-control"
+					vale={this.state.term}
+				></input>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-primary">Submit</button>
 				</span>
