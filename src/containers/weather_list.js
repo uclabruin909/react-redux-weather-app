22d259205de7ea210a8a6569276d5cd3811fb267
Kevin Kim <kevintaekim80@gmail.com>
2018-08-13 18:04:05 -0700

    correct render method to return value

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 
 class WeatherList extends Component {
 	render() {
-		<table className="table table-hover">
-			<thead>
-				<tr>
-					<th>City</th>
-				</tr>
-			</thead>
-			<tbody>
-				
-			</tbody>
-		</table>
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
+		);
 	}
 }
 
